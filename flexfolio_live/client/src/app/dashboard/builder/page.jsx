df982b99391bded2f7ce039094ac7d9178025b5a
Harsh kumar kumawat <kumawatharsh480@gmail.com>
2026-05-21 22:10:21 +0530

    frontend issue fix on versel

---
+++ flexfolio_live/client/src/app/dashboard/builder/page.jsx
@@ -0,0 +1,15 @@
+export default function CreatePortfolioPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-3">
+        Builder Portfolio
+      </h1>
+
+      <p className="text-neutral-400 mb-8">
+        working....
+      </p>
+
+      {/* Your Builder Here */}
+    </div>
+  );
+}
